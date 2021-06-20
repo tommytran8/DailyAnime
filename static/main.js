f3c9153476ed4a51e44d57630494e48b6613a9c8
tommytran8 <trantommy71@gmail.com>
2021-06-20 14:08:08 -0700

    finished search bar
    - search bar will display anime according to searched title
    - not case sensitive

--- static/main.js
+++ static/main.js
@@ -6,9 +6,16 @@ document.getElementById("search").addEventListener("click", ()=>{
     document.getElementById("ALL").click();
 });
 
-document.getElementById("search").addEventListener("change", (inp)=>{
-    console.log(document.getElementById("search").innerText);
-})
+//setting search bar
+document.getElementById("search").addEventListener("input", ()=>{
+    const searched = document.getElementById("search").value;
+
+    for (const component of document.getElementsByClassName("col-sm-12")) {
+        // if anime title matches what is searched, display anime
+        (component.id).toLowerCase().includes(searched) ? component.style.display = "block" : component.style.display = "none";
+    }
+    document.getElementsByClassName("col-sm-12")[0].style.display = "block";
+});
 
 fetch(`${window.location.href}data`)
     .then(response => response.json())
@@ -16,11 +23,12 @@ fetch(`${window.location.href}data`)
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "ALL"]
         const row = document.getElementsByClassName("row")[0];
         
+        //creates anime components and associate class with day
         data.forEach(anime => {
             let component = document.createElement('anime-container');
-            let score = anime["score"].startsWith("N/A") ? "N/A" : anime["score"].slice(0, anime["score"].indexOf(".") + 3);
-            component.setAttribute("class", `col-sm-12 ${anime["day"].slice(0, anime["day"].indexOf("y") + 1)}`);
-            component.setAttribute("id", anime["name"]);
+            let score = anime["score"].startsWith("N/A") ? "N/A" : anime["score"].slice(0, anime["score"].indexOf(".") + 3); //gets score to 3 figures
+            component.setAttribute("class", `col-sm-12 ${anime["day"].slice(0, anime["day"].indexOf("y") + 1)}`); //sets class to col-sm-12 and day
+            component.setAttribute("id", anime["name"]); // id used in seacrh bar
             component.link.href = anime["url"];
             component.link.target = "_blank";
             component.name.textContent = `Title: ${anime["name"]}`;
@@ -33,6 +41,8 @@ fetch(`${window.location.href}data`)
             row.append(component);
 
         });
+
+        //tabs will display anime according to day it broadcasts
         days.forEach(day => {
             document.getElementById(day).addEventListener('click', ()=>{
                 if (day == "ALL") {
@@ -55,6 +65,8 @@ fetch(`${window.location.href}data`)
                 }
             });
         });
+
+        //sets inital tab to current day of the week
         const today = new Date();
         document.getElementById(`${days[today.getDay()]}`).click();
     });         
